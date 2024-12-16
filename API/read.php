363f8fe6f26bff5353ec72f815640eee6ed5734b
Jesús Valverde <109575758+Jesus-Valverde@users.noreply.github.com>
2024-12-16 00:39:51 -0700

    Agregada la carpeta API con el archivo read.php

---
+++ API/read.php
@@ -0,0 +1,43 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../Configuracion/DataBase.php';
+include_once '../Clases/Productos.php';
+
+$objBaseDatos = new DataBase();
+$db = $objBaseDatos->getConnection();
+
+$objProductos = new Productos($db);
+
+$stmt = $objProductos->getProductos();
+$totalProductos = $stmt->rowCount();
+
+
+if ($totalProductos > 0) {
+    $arregloProductos = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $e = array(
+            "idProducto" => $idProducto,
+            "nombre" => $nombre,
+            "descripcion" => $descripcion,
+            "precioCompra" => $precioCompra,
+            "precioVenta" => $precioVenta,
+            "existencia" => $existencia
+        );
+
+        array_push($arregloProductos, $e);
+    }
+
+    echo json_encode($arregloProductos);
+
+} else {
+    http_response_code(404);
+    echo json_encode(
+        array("message" => "No se encontraron productos.")
+    );
+}
+?>
